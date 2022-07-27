0d52a03b80182d826295bf8ef4636d4110fe21e7
Dastan Shokimov <dastan.shokimov@jits-4-5.jits.ipa.dc>
2022-07-27 16:10:54 +0600

    First Commit

---
+++ src/main/java/kz/shokimov/edu/springiorest/SpringiorestApplication.java
@@ -0,0 +1,13 @@
+package kz.shokimov.edu.springiorest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringiorestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringiorestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/shokimov/edu/springiorest/SpringiorestApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.shokimov.edu.springiorest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringiorestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
